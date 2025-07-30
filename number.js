978cf01e40e83d40afebc0f2d0c7a6fc4f16503a
sheikhrasel35 <abushaidrasel@gmail.com>
2025-07-30 23:52:46 +0600

    number.js

---
+++ number.js
@@ -0,0 +1,15 @@
+var orangePrice = 20;
+var chocolate = 9.5;
+// var applePrice=parseInt('20');
+var applePrice = parseFloat("20.6");
+
+console.log(typeof orangePrice);
+console.log(typeof applePrice);
+console.log(applePrice + orangePrice);
+
+var first = 0.1;
+
+var second = 0.2;
+var sum = first + second;
+
+console.log(sum.toFixed(3));
