5ed80c39748188dd9c975d87fc68dd65b582c342
sheikhrasel35 <abushaidrasel@gmail.com>
2025-07-30 23:54:29 +0600

    sorthand.js

---
+++ sorthand.js
@@ -0,0 +1,23 @@
+var price = 40;
+// var price=price+10;
+price += 10;
+console.log(price);
+
+var orange = 70;
+// var orange=orange-20;
+orange -= 20;
+console.log(orange);
+
+var mango = 20;
+// var mango=mango*10;
+
+mango *= 10;
+console.log(mango);
+
+// modulus
+var potato = 30;
+var price = 4;
+
+var modulus = potato % price;
+
+console.log(modulus);
