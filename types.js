959f2df0fd6b69ecd493a79ee310a08465d36e78
sheikhrasel35 <abushaidrasel@gmail.com>
2025-07-30 23:49:21 +0600

    types.js

---
+++ types.js
@@ -0,0 +1,15 @@
+// string types
+var name = "rasel";
+console.log(name);
+
+var address = "bogura";
+
+var country = "Bangladesh";
+var price = 339;
+// boolean
+var isRich = true;
+console.log(isRich);
+var passed = false;
+console.log(typeof passed);
+console.log(typeof address);
+console.log(typeof price);
